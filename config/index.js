76bac8df5adc202cf4664c3e685f1c0d7f213636
7 <>
2022-04-07 15:29:26 +0800

    feat: 编译优化

--- config/index.js
+++ config/index.js
@@ -9,7 +9,11 @@ const config = {
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
-  plugins: [],
+  /**
+   * @title 优化编译速度
+   * @link https://taro-docs.jd.com/taro/docs/compile-optimized#%E4%BC%98%E5%8C%96%E7%BC%96%E8%AF%91%E9%80%9F%E5%BA%A6
+   */
+  plugins: ['taro-plugin-compiler-optimization'],
   defineConstants: {},
   copy: {
     patterns: [],
@@ -35,6 +39,30 @@ const config = {
           generateScopedName: '[name]__[local]___[hash:base64:5]',
         },
       },
+    },
+    /**
+     *
+     * @title 通过webpackChain配置解决开发环境下小程序包体积过大无法进行预览上传的问题(不影响devtools的使用)
+     * @link https://taro-docs.jd.com/taro/docs/compile-optimized#%E8%A7%A3%E5%86%B3%E5%8C%85%E4%BD%93%E7%A7%AF%E8%BF%87%E5%A4%A7%E6%97%A0%E6%B3%95%E8%BF%9B%E8%A1%8C%E9%A2%84%E8%A7%88%E7%9A%84%E9%97%AE%E9%A2%98
+     */
+    webpackChain: (chain, webpack) => {
+      chain.merge({
+        plugin: {
+          install: {
+            plugin: require('terser-webpack-plugin'),
+            args: [
+              {
+                terserOptions: {
+                  compress: true, // 默认使用terser压缩
+                  // mangle: false,
+                  keep_classnames: true, // 不改变class名称
+                  keep_fnames: true, // 不改变函数名称
+                },
+              },
+            ],
+          },
+        },
+      });
     },
   },
   h5: {
